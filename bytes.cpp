fb4d576538a748b8ab8cf8a3029473e5a087d72a
Hasan-Ahmed <ihasan5639@gmail.com>
2026-03-20 15:56:58 +0300

    add kilobyte converter project
    simple cpp program to convert kilobytes to

---
+++ bytes.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main(){
+	
+	
+cout<<"Hi sir plz enter the numbar:";
+	double kilobyt;
+	cin >> kilobyt;
+	double byte = kilobyt * 1024;
+	double bit = byte * 8;
+	cout << "the number of kilobyt is: " << kilobyt << " kilobyt\n";
+	cout << "the number of byte is: " << byte << " byte\n";
+	cout << "the number of bit is: " << bit << " bit\n";
+
+
+
+
+	
+	
+	
+	
+	
+}
